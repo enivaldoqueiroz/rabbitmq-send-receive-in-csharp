243b5d913dc4294ed985fdb814f162e24bbdb637
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-11-02 18:49:11 -0300

    Add project files.

---
+++ Receive/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+Console.WriteLine(value: " [*] Argumento mensagens.");
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(model: channel);
+consumer.Received += (model, basicDeliverEventArgs) =>
+{
+    byte[] body = basicDeliverEventArgs.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+
+    Aluno aluno = JsonSerializer.Deserialize<Aluno>(message);
+
+    Console.WriteLine(value: $" [x] Recebido: {message}");
+
+};
+
+channel.BasicConsume(queue: "hello",
+                     autoAck: true,
+                     consumer: consumer);
+Console.WriteLine(value: " Aperte [ENTER] para sair");
+Console.ReadLine();
+
+public class Aluno
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+Console.WriteLine(value: "Digite sua mansagem e aperte <ENTER>");
+
+while (true)
+{
+    string message = Console.ReadLine();
+
+    if (message == "")
+        break;
+
+    Aluno aluno = new Aluno() { Id = 1, Nome = "Enivaldo" };
+    message = JsonSerializer.Serialize(aluno);
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: string.Empty,
+                         routingKey: "hello",
+                         basicProperties: null,
+                         body: body);
+
+    Console.WriteLine(value: $"[x] Enviado {message}");
+}
+
+public class Aluno
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+}
